6a9507514babb5e837e5eebcaf7017e40e91f27e
BassMaster629 <oceans4496@gmail.com>
2024-04-10 10:11:56 -0700

    begin placeholders
    Co-authored-by: Many <Manydifferentways@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
+question = 1
+
+st.set_page_config(
+    page_title='Career Survey',
+    layout='wide',
+    page_icon='🎓',
+)
+
 st.title('Career Recommendation System')
+st.header('Take the survey and we will recommend you the best career!')
+with st.form('User Input'):
+    if st.form_submit_button('Submit'):
+        pass
+
+if question == 1:
+        if st.button("place holder"):
+            pass
+
+st.header('Recommending place holder')
