550a8de489e18b280eebd2970a88513e6ecaec21
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-04-12 20:19:24 -0700

    basic algorithm

--- main.py
+++ main.py
@@ -8,27 +8,38 @@ st.set_page_config(
     page_icon='🎓',
 )
 
-if 'expanded' not in st.session_state:
-    st.session_state.expanded = True
-if 'filled_out' not in st.session_state:
-    st.session_state.filled_out = False
+CAREER_OPTIONS = {'Aerospace':[1, 2, 3], 'Engineer':[1, 3], 'Architect':[1, 4], 'Military':[2, 3], 'Healthcare':[1, 2, 3, 5], 'Computer Science':[4], 'Environmental Science':[1, 3], 'Cullinary':[2, 4, 5], 'Agriculture':[2, 3], 'Teaching':[1, 4, 5], 'Construction':[2, 3, 4], 'Finance':[1, 4], 'Marketing':[1, 4]}
+
+def eliminate_options(responses: list, options: dict):
+    ranked_options = []
+    for i in range(len(responses)):
+        for j in range(len(options)):
+            if responses[i] in list(options.values())[j] and list(options.keys())[j] not in ranked_options:
+                removed = list(options.keys()).pop(j)
+                ranked_options.append(removed)
+    for i in range(len(list(options.keys()))):
+        if list(options.keys())[i] not in ranked_options:
+            ranked_options.append(list(options.keys())[i])
+    return ranked_options
 
 st.title('Career Recommendation System')
 st.write("Welcome to our career recommendation tool! 🌟 Please keep in mind that our algorithm is a work in progress, crafted over just four days with limited data for training. While we've done our best to provide personalized suggestions based on your survey responses, it's important to acknowledge that our results may not be as accurate as we aspire them to be. We're continuously refining our system to improve accuracy and relevance. Thank you for your understanding and patience as we evolve!")
 st.divider()
 st.subheader('Please answer the following questions to get started.')
-with st.expander('Survey', expanded=st.session_state.expanded):
+with st.expander('Survey'):
     with st.form('Survey'):
         q1 = st.toggle('I am willing to attend a 2-year or 4-year university.')
         q2 = st.toggle('I am willing to work a job with high levels of physical activity.')
         q3 = st.toggle('I am willing to work in an outdoor environment.')
         q4 = st.toggle('I am willing to work personally with clients.')
         q5 = st.toggle('I am willing to work with children.')
+        responses = [q1, q2, q3, q4, q5]
+        interests = st.multiselect('Select all that interest you:', ['Aviation', 'Space', 'Science', 'Technology', 'Reading', 'Travel', 'Building', 'Drawing/Art', 'Gardening', 'The Stock Market', 'Running a Business'])
         if st.form_submit_button('Submit'):
-            st.session_state.expanded = False
-            st.session_state.filled_out = True
-            st.rerun()
-if st.session_state.filled_out:
-    with st.spinner('Please wait while we process your responses...'):
-        sleep(3)
-    st.title('boobies')
+            response_numbers = []
+            for i in range(len(responses)):
+                if not responses[i]:
+                    response_numbers.append(i + 1)
+            print(response_numbers)
+            with st.spinner('Please wait while we process your responses...'):
+                st.write(eliminate_options(response_numbers, CAREER_OPTIONS)[-3:])
